31545880f83dbf6d9f9c5e4a692418f88c37625e
brlopez192 <brlopez192@gmail.com>
2025-02-23 00:28:47 -0500

    changes

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+// import { GiftedChat } from "react-native-gifted-chat";
+
+
 
 const Chat = ({ route, navigation }) => {
   const { name } = route.params;
@@ -12,30 +14,30 @@ const Chat = ({ route, navigation }) => {
 
   useEffect(() => {
     navigation.setOptions({ title: name });
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello Developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        }
-      }
-    ])
+    // setMessages([
+    //   {
+    //     _id: 1,
+    //     text: "Hello Developer",
+    //     createdAt: new Date(),
+    //     user: {
+    //       _id: 2,
+    //       name: "React Native",
+    //       avatar: "https://placeimg.com/140/140/any",
+    //     }
+    //   }
+    // ])
   }, []);
 
  return (
 
    <View style={[styles.container, {backgroundColor:backgroundColor}]}>
-    <GiftedChat
+    {/* <GiftedChat
     messages={messages}
     onSend={messages => onSend(messages)}
     user={{
       _id: 1
     }}
-    />
+    /> */}
    </View>
  );
 }
