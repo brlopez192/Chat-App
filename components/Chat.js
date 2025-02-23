e4bf03c74ae86380d30983e1e5edf67bf32febde
brlopez192 <brlopez192@gmail.com>
2025-02-23 02:30:30 -0500

    5.2 code complete

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import {GiftedChat} from 'react-native-gifted-chat';
+import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
 
 
 
@@ -13,40 +13,63 @@ const Chat = ({ route, navigation }) => {
   }
 
   useEffect(() => {
-    navigation.setOptions({ title: name });
     setMessages([
       {
         _id: 1,
-        text: "Hello Developer",
+        text: 'Hello developer',
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        }
-      }
-    ])
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
 
- return (
-
-   <View style={[styles.container, {backgroundColor:backgroundColor}]}>
-    <GiftedChat
-    messages={messages}
-    onSend={messages => onSend(messages)}
-    user={{
-      _id: 1
-    }}
-    /> 
-   </View>
- );
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
+  return (
+    <View style={[styles.messContainer, {backgroundColor: backgroundColor}]}>
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+      { Platform.OS === "ios" ?  <KeyboardAvoidingView behavior="padding" /> : null }
+    </View>
+  )
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  messContainer: {
+    flex: 1
   }
 });
 
